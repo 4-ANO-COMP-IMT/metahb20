d766485687b09e4ed24fe676cc9659c7af23ca58
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-05-26 17:42:26 -0300

    feat(user): Create user usecase

---
+++ back/mss_user/src/modules/createUser/createUserUsecase.js
@@ -0,0 +1,17 @@
+import { User } from "../../shared/domain/entities/user.js";
+import { UserRepositoryMock } from "../../shared/infra/repositories/userRepositoryMock.js";
+import { v4 as uuidv4 } from "uuid";
+
+export class CreateUserUsecase {
+  constructor(repo) {
+    this.repo = repo;
+  }
+
+  async call(name, email, favoriteGenre, favoriteBook) {
+    const userId = uuidv4();
+
+    const user = new User(userId, name, email, favoriteGenre, favoriteBook);
+
+    return this.repo.createUser(user);
+  }
+}
