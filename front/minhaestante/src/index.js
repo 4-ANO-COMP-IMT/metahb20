95b1814cd361ceab40d21691d1871b557daa4def
RaphaelRAY <raphaelfr01@gmail.com>
2024-06-01 17:46:39 -0300

    feat(minhaestante): Add Cadastro page and render it in index.js
    Co-Authored-By: Nicole M. <145381275+nicmf@users.noreply.github.com>

---
+++ front/minhaestante/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './pages/Cadastro';
+import Cadastro from './pages/Cadastro';
+
+const App = () => {
+  return <Cadastro/>;
+}
+
+ReactDOM.render(
+<App />, 
+document.getElementById('root'));
---
+++ front/minhaestante/src/pages/Cadastro.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Cadastro extends React.Component{
+    render(){
+        return
+    }
+}
+
+export default Cadastro;
