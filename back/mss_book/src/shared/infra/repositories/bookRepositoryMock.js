864f4c644fe8b36b5423c141a1fc18a8a90b6113
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-06-03 00:44:27 -0300

    feat(book): add book repository mock

--- back/mss_book/src/shared/infra/repositories/bookRepositoryMock.js
+++ back/mss_book/src/shared/infra/repositories/bookRepositoryMock.js
@@ -22,8 +22,30 @@ export class BookRepositoryMock {
         "J. R. R. Tolkien",
         1000,
         GENRES.FICCAO,
-        new Date(-649029540000),
+        new Date(-486853627000),
         "Allen & Unwin",
+        5
+      ),
+      new Book(
+        "6c9ea682-36bb-58ba-b590-eaf2a9c7e0c6",
+        "Diário de Um Banana",
+        1,
+        "Jeff Kinney",
+        300,
+        GENRES.CONTO,
+        new Date(1211165573000),
+        "Amulet Books",
+        5
+      ),
+      new Book(
+        "7c9ea682-36bb-58ba-b590-eaf2a9c7e0c6",
+        "O Pequeno Príncipe",
+        1,
+        "Antoine de Saint-Exupéry",
+        100,
+        GENRES.POEMA,
+        new Date(-843858427000),
+        "Reynal & Hitchcock",
         5
       ),
     ];
