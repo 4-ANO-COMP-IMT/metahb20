e50851d64be1ffdbfea166063afbbba6e0c3cf69
RaphaelRAY <raphaelfr01@gmail.com>
2024-06-02 21:55:41 -0300

    feat(deleteUser): Add deleteUser route to userRoutes

--- back/mss_user/src/routes/userRoutes.js
+++ back/mss_user/src/routes/userRoutes.js
@@ -1,20 +1,27 @@
-import express from "express";
-import { createUserPresenter } from "../modules/createUser/createUserPresenter.js";
-import { getUserPresenter } from "../modules/getUser/getUserPresenter.js";
-import { HttpRequest } from "../shared/helpers/externalInterfaces/httpModels.js";
+import express from 'express';
+import { createUserPresenter } from '../modules/createUser/createUserPresenter.js';
+import { getUserPresenter } from '../modules/getUser/getUserPresenter.js';
+import { deleteUserPresenter } from '../modules/deleteUser/deleteUserPresenter.js';
+import { HttpRequest } from '../shared/helpers/externalInterfaces/httpModels.js';
 
 const routes = express.Router();
 
-routes.post("/user", async (req, res) => {
-  const event = new HttpRequest(req.body);
-  const response = await createUserPresenter(event);
-  res.status(response.statusCode).json(response.body);
+routes.post('/user', async (req, res) => {
+	const event = new HttpRequest(req.body);
+	const response = await createUserPresenter(event);
+	res.status(response.statusCode).json(response.body);
 });
-routes.get("/user/:id", async (req, res) => {
-  const event = new HttpRequest({ userId: req.params.id });
-  console.log(event);
-  const response = await getUserPresenter(event);
-  res.status(response.statusCode).json(response.body);
+routes.get('/user/:id', async (req, res) => {
+	const event = new HttpRequest({ userId: req.params.id });
+	console.log(event);
+	const response = await getUserPresenter(event);
+	res.status(response.statusCode).json(response.body);
+});
+routes.delete('/user/:id', async (req, res) => {
+	const event = new HttpRequest({ userId: req.params.id });
+	console.log(event);
+	const response = await deleteUserPresenter(event);
+	res.status(response.statusCode).json(response.body);
 });
 
 export default routes;
