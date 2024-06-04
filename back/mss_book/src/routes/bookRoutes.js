535a6139f72e3c4ee6cb82181734565448d3c686
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-06-03 22:09:00 -0300

    feat: Add route for retrieving a book by ID

---
+++ back/mss_book/src/routes/bookRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { getBookPresenter } from "../modules/getBook/getBookPresenter.js";
+import { HttpRequest } from "../shared/helpers/externalInterfaces/httpModels.js";
+
+const routes = express.Router();
+
+routes.get("/book/:bookId", async (req, res) => {
+  const request = new HttpRequest({
+    bookId: req.params.bookId,
+  });
+
+  const response = await getBookPresenter(request);
+
+  res.status(response.statusCode).send(response.body);
+});
