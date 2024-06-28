1225050bfd4d456fb8e249c0cf5d04dc9ac19fae
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-06-28 16:55:50 -0300

    feat(updatUser): Add updateUser to routes

--- back/mss_user/src/routes/userRoutes.js
+++ back/mss_user/src/routes/userRoutes.js
@@ -1,27 +1,35 @@
-import express from 'express';
-import { createUserPresenter } from '../modules/createUser/createUserPresenter.js';
-import { getUserPresenter } from '../modules/getUser/getUserPresenter.js';
-import { deleteUserPresenter } from '../modules/deleteUser/deleteUserPresenter.js';
-import { HttpRequest } from '../shared/helpers/externalInterfaces/httpModels.js';
+import express from "express";
+import { createUserPresenter } from "../modules/createUser/createUserPresenter.js";
+import { getUserPresenter } from "../modules/getUser/getUserPresenter.js";
+import { deleteUserPresenter } from "../modules/deleteUser/deleteUserPresenter.js";
+import { HttpRequest } from "../shared/helpers/externalInterfaces/httpModels.js";
+import { updateUserPresenter } from "../modules/updateUser/updateUserPresenter.js";
 
 const routes = express.Router();
 
-routes.post('/user', async (req, res) => {
-	const event = new HttpRequest(req.body);
-	const response = await createUserPresenter(event);
-	res.status(response.statusCode).json(response.body);
+routes.post("/user", async (req, res) => {
+  const event = new HttpRequest(req.body);
+  const response = await createUserPresenter(event);
+  res.status(response.statusCode).json(response.body);
 });
-routes.get('/user/:id', async (req, res) => {
-	const event = new HttpRequest({ userId: req.params.id });
-	console.log(event);
-	const response = await getUserPresenter(event);
-	res.status(response.statusCode).json(response.body);
+routes.get("/user/:id", async (req, res) => {
+  const event = new HttpRequest({ userId: req.params.id });
+  console.log(event);
+  const response = await getUserPresenter(event);
+  res.status(response.statusCode).json(response.body);
 });
-routes.delete('/user/:id', async (req, res) => {
-	const event = new HttpRequest({ userId: req.params.id });
-	console.log(event);
-	const response = await deleteUserPresenter(event);
-	res.status(response.statusCode).json(response.body);
+routes.delete("/user/:id", async (req, res) => {
+  const event = new HttpRequest({ userId: req.params.id });
+  console.log(event);
+  const response = await deleteUserPresenter(event);
+  res.status(response.statusCode).json(response.body);
+});
+
+routes.put("/user", async (req, res) => {
+  const event = new HttpRequest(req.body);
+  console.log(event);
+  const response = await updateUserPresenter(event);
+  res.status(response.statusCode).json(response.body);
 });
 
 export default routes;
