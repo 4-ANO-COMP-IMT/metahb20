0bb25b4e2e8b52cefa7f7bb9db73bb65f610df98
nicmf <145381275+nicmf@users.noreply.github.com>
2024-06-28 19:28:25 -0300

    test(minhaestante): test LogIn page

--- front/minhaestante/src/index.js
+++ front/minhaestante/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Registration from "./pages/Registration";
+import LogIn from "./pages/LogIn";
 
 const App = () => {
-	return <Registration />;
+	return <LogIn />;
 };
 
 createRoot(document.getElementById("root")).render(<App />);
