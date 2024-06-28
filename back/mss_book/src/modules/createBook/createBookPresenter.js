be061393977263aeb893b172251c5235c746a4e8
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-06-28 14:30:39 -0300

    feat(createBook): add getBookPresenter function

---
+++ back/mss_book/src/modules/createBook/createBookPresenter.js
@@ -0,0 +1,15 @@
+import { CreateBookController } from "./createBookController.js";
+import { CreateBookUsecase } from "./createBookUsecase.js";
+import { BookRepositoryMock } from "../../shared/infra/repositories/bookRepositoryMock.js";
+import { HttpResponse } from "../../shared/helpers/externalInterfaces/httpModels.js";
+
+const repo = new BookRepositoryMock();
+const usecase = new CreateBookUsecase(repo);
+const controller = new CreateBookController(usecase);
+
+export async function getBookPresenter(event) {
+  const response = await controller.call(event);
+  const httpResponse = new HttpResponse(response?.statusCode, response?.body);
+
+  return httpResponse;
+}
