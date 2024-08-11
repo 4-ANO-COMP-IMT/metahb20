d900f1b1bb000b5b56b7fe9b906cf98664709399
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-11 16:46:40 -0300

    feat(minhaestante): Add BookCard component
    This commit adds the BookCard component to the minhaestante project. The component displays book information such as title, author, genre, publisher, and rating. It also includes a button for further actions. This component will be used to render individual book cards in the application.

---
+++ front/minhaestante/src/components/book/BookCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+//book infomations that a want to show
+// //"367f3e10-4649-5e8e-acc6-4669644a520e",
+// "1984",
+// 1,
+// "George Orwellr",
+// 200,
+// GENRES.FICCAO,
+// -649029540000,
+// "Secker and Warburg",
+// 5
+class BookCard extends React.Component {
+	render() {
+		const { title, author, genre, publisher, rating } = this.props;
+
+		return (
+			<div className="card">
+				<div className="row">
+					<div className="card-body">
+						<h5 className="card-title">{title}</h5>
+						<p className="card-text">
+							Author: {author}
+							<br />
+							Genre: {genre}
+							<br />
+							Publisher: {publisher}
+							&nbsp;&nbsp;&nbsp; Rating: {rating}
+						</p>
+						<div className="text">
+							<button className="btn btn-primary">Button</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default BookCard;
