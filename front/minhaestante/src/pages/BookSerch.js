8048b161f45fb2db65e78806c149e79b52329ff6
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-11 17:13:48 -0300

    feat(minhaestante): Add BookSearch page not working yet

---
+++ front/minhaestante/src/pages/BookSerch.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import BookCard from "../components/book/BookCard";
+
+class BookSearch extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			books: [],
+			search: "",
+		};
+	}
+
+	onChangeSearch = async (event) => {
+		this.setState({ search: event.target.value });
+	};
+
+	onClickSearch = async (event) => {
+		const response = await axios.get(`http://localhost:4000/mssbook`);
+		this.setState({ books: response.data.items });
+	};
+
+	render() {
+		return (
+			<div className="container-sm mt-2 border border-dark ">
+				<div className="row justify-content-center headline ">
+					<div className="col-6">
+						<h1 className="display-5 text-center ">
+							<strong>Search</strong>
+						</h1>
+					</div>
+				</div>
+
+				<div className="row justify-content-center background-form">
+					<div className="col-9">
+						<div className="row form-group justify-content-around m-3 ">
+							<input
+								className="form-control col-6"
+								type="text"
+								placeholder="Search"
+								value={this.state.search}
+								onChange={this.onChangeSearch}
+							/>
+							<button
+								className="btn btn-primary col-2 button-style"
+								onClick={this.onClickSearch}
+							>
+								Search
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div className="row justify-content-center background-form">
+					<div className="col-9">
+						<div className="row form-group justify-content-around m-3 ">
+							<div className="col-3">
+								<BookCard
+									id="367f3e10-4649-5e8e-acc6-4669644a520e"
+									title="1984"
+									edition={1}
+									author="George Orwell"
+									pages={200}
+									genre={"FICCAO"}
+									publicationDate={-649029540000}
+									publisher="Secker and Warburg"
+									rating={5}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default BookSearch;
