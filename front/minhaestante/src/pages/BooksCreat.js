cdb62e38d019063243c9134faf0d300061da1191
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-15 22:16:50 -0300

    chore(minhaestante): Add creationbook form
    Criado um componente parecido com a registrar o componente usuario so que agora para o livro
    
    Co-Authored-By: Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>

---
+++ front/minhaestante/src/pages/BooksCreat.js
@@ -0,0 +1,144 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { GENRES } from "../shared/domain/enums/genresEnum";
+import axios from "axios";
+import InputField from "../components/registration/InputField";
+import SelectField from "../components/registration/SelectField";
+import SucessMessage from "../components/registration/SucessMessage";
+import ErrorMessage from "../components/registration/ErrorMessage";
+import errorMessageTranslator from "../shared/error/ErrorManager";
+import "../styles/style.css";
+
+class RegistrationForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+            bookId: "",
+            title: "",
+            edition: "",
+            autor: "",
+            pages: "",
+            genre: "",
+            publishDate: "",
+            publiser: "",
+            rating: ""
+		};
+	}
+
+	onClickCadastrar = async (event) => {
+		event.preventDefault();
+
+		const user = {
+			name: this.state.name,
+			email: this.state.email,
+			favoriteGenres: this.state.favoriteGenres,
+			favoriteBook: this.state.favoriteBook,
+		};
+
+		const response = await axios
+			.post("http://localhost:4000/api/user", user)
+			.then((res) => {
+				this.setState({
+					name: "",
+					email: "",
+					favoriteGenres: "",
+					favoriteBook: "",
+					successMessage: "Usuário cadastrado com sucesso!",
+					errorMessage: "",
+				});
+
+				setTimeout(() => {
+					this.setState({ successMessage: "" });
+				}, 10000);
+			})
+			.catch((error) => {
+				console.error(error);
+
+				this.setState({
+					errorMessage: errorMessageTranslator.translateErrorMessage(
+						error.request.responseText
+					),
+					successMessage: "",
+				});
+			});
+	};
+
+	onClickLogin = (event) => {
+		event.preventDefault();
+	};
+
+	render() {
+		return (
+			<div className="container-sm mt-2 border border-dark ">
+				<div className="row justify-content-center headline ">
+					<div className="col-6">
+						<h1 className="display-5 text-center ">
+							<strong>CADASTRO</strong>
+						</h1>
+					</div>
+				</div>
+
+				<div className="row justify-content-center background-form">
+					<div className="col-9">
+						<form>
+							<InputField
+								label="Nome"
+								type="text"
+								value={this.state.name}
+								onChange={(e) => this.setState({ name: e.target.value })}
+							/>
+
+							<InputField
+								label="Email"
+								type="email"
+								value={this.state.email}
+								onChange={(e) => this.setState({ email: e.target.value })}
+							/>
+
+							<SelectField
+								label="Gêneros Favoritos"
+								options={Object.values(GENRES)}
+								value={this.state.favoriteGenres}
+								onChange={(e) =>
+									this.setState({ favoriteGenres: e.target.value })
+								}
+							/>
+
+							<InputField
+								label="Livro Favorito"
+								type="text"
+								value={this.state.favoriteBook}
+								onChange={(e) =>
+									this.setState({ favoriteBook: e.target.value })
+								}
+							/>
+
+							{this.state.successMessage && (
+								<SucessMessage message={this.state.successMessage} />
+							)}
+
+							{this.state.errorMessage && (
+								<ErrorMessage message={this.state.errorMessage} />
+							)}
+
+							<div className="row form-group justify-content-around m-3 ">
+								<button
+									className="btn btn-primary col-4 button-style"
+									onClick={this.onClickCadastrar}
+								>
+									Cadastrar
+								</button>
+
+								<button className="btn btn-primary col-4 button-style">
+									Entrar
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default RegistrationForm;
