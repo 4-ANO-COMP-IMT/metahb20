43e6ac40cbed3c003464a77aef6c145dddd3aa96
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-18 14:50:24 -0300

    feat(front): Add routes for Registration, Login, BookSearch, and BookForm pages

--- front/minhaestante/src/index.js
+++ front/minhaestante/src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import LogIn from "./pages/LogIn";
 import Test from "./pages/Test";
+import BookSearch from "./pages/BookSerch";
+import BookForm from "./pages/BooksForm";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
-	// return <Registration />;
-	return <Test />;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Registration />} />
+				<Route path="/login" element={<LogIn />} />
+				<Route path="/booksearch" element={<BookSearch />} />
+				<Route path="/bookform" element={<BookForm />} />
+				<Route path="/test" element={<Test />} />
+			</Routes>
+		</Router>
+	);
 };
 
 createRoot(document.getElementById("root")).render(<App />);
