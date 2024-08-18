10bc3513125a76ded0c271c67700542360671c0c
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-17 22:07:28 -0300

    chore(back): Add cors middleware for handling cross-origin requests

--- back/mss_book/src/app.js
+++ back/mss_book/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import routes from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/mssbook", routes);
 
