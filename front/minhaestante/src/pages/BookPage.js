58d1794cd2d56b5c815e9439aa92d4a2ccce03eb
Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-24 12:07:11 -0300

    feat(minhaestante): Add BookUpdateForm component for updating book information

--- front/minhaestante/src/pages/BookPage.js
+++ front/minhaestante/src/pages/BookPage.js
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import BookForm from "../components/book/BooksForm";
 import BookSearch from "../components/book/BookSerch";
-
+import BookUpdateForm from "../components/book/BookUpdateForm";
 class BookPage extends React.Component {
 	render() {
 		return (
 			<div>
 				<BookSearch />
 				<BookForm />
+				<BookUpdateForm />
 			</div>
 		);
 	}
