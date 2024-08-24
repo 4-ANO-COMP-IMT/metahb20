d932638e38e9cc3d78cf78f9a57d19317ec2c35a
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-08-24 09:24:09 -0300

    feat(updateBook): add updateBook to routes

--- back/mss_book/src/routes/bookRoutes.js
+++ back/mss_book/src/routes/bookRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getBookPresenter } from "../modules/getBook/getBookPresenter.js";
 import { HttpRequest } from "../shared/helpers/externalInterfaces/httpModels.js";
 import { createBookPresenter } from "../modules/createBook/createBookPresenter.js";
+import { updateBookPresenter } from "../modules/updateBook/updateBookPresenter.js";
 
 const routes = express.Router();
 
@@ -18,6 +19,13 @@ routes.get("/book/:bookId", async (req, res) => {
 routes.post("/book", async (req, res) => {
   const request = new HttpRequest(req.body);
   const response = await createBookPresenter(request);
+
+  res.status(response.statusCode).send(response.body);
+});
+
+routes.put("/book", async (req, res) => {
+  const request = new HttpRequest(req.body);
+  const response = await updateBookPresenter(request);
 
   res.status(response.statusCode).send(response.body);
 });
