39aabb353c90cfeb1e38d72926a2df5cd1b83a44
Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-24 11:36:53 -0300

    feat(minhaestante): Add BookUpdateForm component for updating book information

--- front/minhaestante/src/pages/Test.js
+++ front/minhaestante/src/pages/Test.js
@@ -8,11 +8,12 @@ import BookSearch from "../components/book/BookSerch";
 import BookForm from "../components/book/BooksForm";
 import Registration from "../components/registration/Registration";
 import LoginScreen from "../components/registration/LogIn";
-
+import BookUpdateForm from "../components/book/BookUpdateForm";
 class Test extends React.Component {
 	render() {
 		return (
 			<div>
+				<BookUpdateForm />
 				<Registration />
 				<LoginScreen />
 				<BookForm />
