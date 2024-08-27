f9a2a534d0838fb9edf838299340df56e8bf6e72
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-27 14:38:34 -0300

    chore(minhaestante): Update BookPage component to use BookFinder and BookForm components

--- front/minhaestante/src/pages/BookPage.js
+++ front/minhaestante/src/pages/BookPage.js
@@ -1,19 +1,41 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component, useState } from "react";
+import { Modal } from "react-bootstrap";
 import BookForm from "../components/book/BookForm";
-import BookSearch from "../components/book/BookSerch";
-import BookSechUpdateForm from "../components/book/BookSerchUpdateForm";
+import BookFinder from "../components/book/BookAll";
+import { useParams } from "react-router-dom";
 
-class BookPage extends React.Component {
-	render() {
-		return (
+const BookPage = () => {
+	const [showAddModal, setShowAddModal] = useState(false);
+	const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+	const { userId } = useParams();
+
+	const handleShowAdd = () => setShowAddModal(true);
+	const handleCloseAdd = () => setShowAddModal(false);
+
+	return (
+		<div>
 			<div>
-				<BookSearch />
-				<BookForm />
-				<BookSechUpdateForm />
+				<BookFinder userId={userId}>
+					<button
+						className="btn btn-primary button-style2 m-2"
+						onClick={handleShowAdd}
+					>
+						Adicionar novo livro ao acervo
+					</button>
+				</BookFinder>
 			</div>
-		);
-	}
-}
+			<div>
+				{/* Modal Adicionar Livro */}
+				<Modal show={showAddModal} onHide={handleCloseAdd}>
+					<Modal.Header closeButton></Modal.Header>
+					<Modal.Body>
+						<BookForm />
+					</Modal.Body>
+				</Modal>
+			</div>
+		</div>
+	);
+};
 
 export default BookPage;
