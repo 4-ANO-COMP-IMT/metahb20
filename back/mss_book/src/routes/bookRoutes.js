ff15089bf3de1529ceaa19737fcddfc130b9978e
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-08-30 18:25:37 -0300

    feat(bookshelf): Add route to get bookshelf

--- back/mss_book/src/routes/bookRoutes.js
+++ back/mss_book/src/routes/bookRoutes.js
@@ -6,6 +6,7 @@ import { createBookPresenter } from "../modules/createBook/createBookPresenter.j
 import { updateBookPresenter } from "../modules/updateBook/updateBookPresenter.js";
 import { deleteBookPresenter } from "../modules/deleteBook/deleteBookPresenter.js";
 import { getAllBooksPresenter } from "../modules/getAllBooks/getAllBooksPresenter.js";
+import { getBookshelfPresenter } from "../modules/getBookshelf/getBookshelfpresenter.js";
 
 const routes = express.Router();
 const repo = new BookRepositoryMock();
@@ -44,6 +45,16 @@ routes.delete("/book", async (req, res) => {
 routes.get("/books", async (req, res) => {
   const request = new HttpRequest({});
   const response = await getAllBooksPresenter(request, repo);
+
+  res.status(response.statusCode).send(response.body);
+});
+
+routes.get("/bookshelf/:userID", async (req, res) => {
+  const request = new HttpRequest({
+    userID: req.params.userID,
+  });
+
+  const response = await getBookshelfPresenter(request, repo);
 
   res.status(response.statusCode).send(response.body);
 });
