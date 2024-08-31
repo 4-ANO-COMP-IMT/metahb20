2439d5aae5611187dd4ebdc9c2602b4c3cb15244
Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-31 12:04:47 -0300

    feat(minhaestante): Update BookShelfPage.js to improve table layout and add table title

--- front/minhaestante/src/pages/BookShelfPage.js
+++ front/minhaestante/src/pages/BookShelfPage.js
@@ -31,32 +31,36 @@ const BookShelfPage = () => {
 
 			<div className="row justify-content-center background-form">
 				<div className="col-11 mt-2">
-					<table className="table table-rounded responsive-table">
-						<thead className="thead-dark">
-							<tr>
-								<th scope="col">Title</th>
-								<th scope="col">Author</th>
-								<th scope="col">Pages</th>
-								<th scope="col">Genre</th>
-								<th scope="col">Rating</th>
-								<th scope="col" />
-							</tr>
-						</thead>
-						<tbody>
-							<RowBookshelfComponent
-								userId={userId}
-								id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
-							/>
-							<RowBookshelfComponent
-								userId={userId}
-								id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
-							/>
-							<RowBookshelfComponent
-								userId={userId}
-								id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
-							/>
-						</tbody>
-					</table>
+					<div>
+						<h1 className="display-6 text-center table-title">Livros</h1>
+						<table className="table table-rounded responsive-table">
+							<thead className="thead-dark">
+								<tr>
+									<th scope="col">Title</th>
+									<th scope="col">Author</th>
+									<th scope="col">Pages</th>
+									<th scope="col">Genre</th>
+									<th scope="col">Rating</th>
+									<th scope="col" />
+								</tr>
+							</thead>
+							<tbody>
+								<RowBookshelfComponent
+									userId={userId}
+									id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
+								/>
+								<RowBookshelfComponent
+									userId={userId}
+									id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
+								/>
+								<RowBookshelfComponent
+									userId={userId}
+									id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
+								/>
+							</tbody>
+						</table>
+					</div>
+
 					<div className="row justify-content-center background-form">
 						<button
 							className="col-2 btn btn-primary button-style2 m-2"
