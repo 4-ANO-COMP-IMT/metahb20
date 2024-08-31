1bcfba23c81196ce06aceb74d4f525f6508b6ce3
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-30 21:06:02 -0300

    feat(minhaestante): Add BookShelfPage component for displaying user's bookshelf

---
+++ front/minhaestante/src/pages/BookShelfPage.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import RowBookshelfComponent from "../components/bookshelf/RowBookshelf";
+import "../styles/table.css";
+
+const BookShelfPage = () => {
+	const { userId } = useParams();
+
+	const onClickAddBook = () => {
+		console.log("Adicionar livro a estante");
+		window.location.href = "/books/" + userId;
+	};
+	const onClickUpdateUser = () => {
+		console.log("Adicionar livro a estante");
+		window.location.href = "/user/" + userId;
+	};
+
+	return (
+		<div className="container-xl mt-2 border border-dark ">
+			<div className="row justify-content-center headline ">
+				<div className="col-12">
+					<h1 className="display-5 text-center ">
+						<strong>SUA ESTANTE</strong>
+					</h1>
+				</div>
+			</div>
+			<div className="row justify-content-center background-form">
+				<div className="col-11 mt-2">
+					<table className="table table-rounded responsive-table">
+						<thead className="thead-dark">
+							<tr>
+								<th scope="col">Title</th>
+								<th scope="col">Author</th>
+								<th scope="col">Pages</th>
+								<th scope="col">Genre</th>
+								<th scope="col">Rating</th>
+								<th scope="col" />
+							</tr>
+						</thead>
+						<tbody>
+							<RowBookshelfComponent
+								userId={userId}
+								id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
+							/>
+							<RowBookshelfComponent
+								userId={userId}
+								id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
+							/>
+							<RowBookshelfComponent
+								userId={userId}
+								id={"367f3e10-4649-5e8e-acc6-4669644a520e"}
+							/>
+						</tbody>
+					</table>
+					<div className="row justify-content-center background-form">
+						<button
+							className="col-3 btn btn-primary button-style2 m-2"
+							onClick={onClickAddBook}
+						>
+							Adicionar livro a estante
+						</button>
+						<button
+							className="col-3 btn btn-primary button-style2 m-2"
+							onClick={onClickUpdateUser}
+						>
+							Modificar usuario
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default BookShelfPage;
