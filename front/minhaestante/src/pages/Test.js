ecfdf6dc17580dcbb1f4ec0619c15a8045a4c071
RaphaelRAY <100390575+RaphaelRAY@users.noreply.github.com>
2024-08-30 21:07:39 -0300

    feat(minhaestante): Update Test.js to use BookShelfPage component

--- front/minhaestante/src/pages/Test.js
+++ front/minhaestante/src/pages/Test.js
@@ -3,19 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { GENRES } from "../shared/domain/enums/genresEnum";
 import axios from "axios";
 import SucessMessage from "../components/common/SucessMessage";
-import BookCard from "../components/book/BookCard";
-import BookSearch from "../components/book/BookSerch";
 import BookForm from "../components/book/BookForm";
 import Registration from "../components/user/Registration";
 import LoginScreen from "../components/user/LogIn";
 import BookSechUpdateForm from "../components/book/BookSerchUpdateForm";
 import BookFinder from "../components/book/BookAll";
 import BookPage from "./BookPage";
+import BookShelfPage from "./BookShelfPage";
 class Test extends React.Component {
 	render() {
 		return (
 			<div>
-				<BookPage />
+				<BookShelfPage />
 			</div>
 		);
 	}
