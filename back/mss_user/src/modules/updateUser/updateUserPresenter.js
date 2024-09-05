0e90b73629e6ed28eadae086e3fbdd9d5d34db27
Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>
2024-09-05 16:52:44 -0300

    Revert "fix(minhaestante): Refactor updateUserPresenter to include repository parameter"
    This reverts commit ed0d708cf6d347f0e4324d24860f5e2e4166ee98.

--- back/mss_user/src/modules/updateUser/updateUserPresenter.js
+++ back/mss_user/src/modules/updateUser/updateUserPresenter.js
@@ -3,11 +3,11 @@ import { UpdateUserUsecase } from "./updateUserUsecase.js";
 import { UserRepositoryMock } from "../../shared/infra/repositories/userRepositoryMock.js";
 import { HttpResponse } from "../../shared/helpers/externalInterfaces/httpModels.js";
 
-export async function updateUserPresenter(event, repo) {
-	const usecase = new UpdateUserUsecase(repo);
-	const controller = new UpdateUserController(usecase);
-	const response = await controller.call(event);
-	const httpResponse = new HttpResponse(response?.statusCode, response?.body);
+export async function updateUserPresenter(event) {
+  const usecase = new UpdateUserUsecase(repo);
+  const controller = new UpdateUserController(usecase);
+  const response = await controller.call(event);
+  const httpResponse = new HttpResponse(response?.statusCode, response?.body);
 
-	return httpResponse;
+  return httpResponse;
 }
