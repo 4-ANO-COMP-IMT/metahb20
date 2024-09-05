fb7195db1b4a0f249b171700be95ecdde1dc2e3b
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-09-05 16:29:41 -0300

    fix(mss_user):fix presenter tests

--- back/mss_user/tests/modules/create_user/createUserPresenter.test.js
+++ back/mss_user/tests/modules/create_user/createUserPresenter.test.js
@@ -1,6 +1,7 @@
 import { createUserPresenter } from "../../../src/modules/createUser/createUserPresenter.js";
 import { describe, test, expect } from "vitest";
 import { HttpRequest } from "../../../src/shared/helpers/externalInterfaces/httpModels.js";
+import { UserRepositoryMock } from "../../../src/shared/infra/repositories/userRepositoryMock.js";
 
 describe("Create User Presenter Tests", () => {
   test("Test Create User Presenter", async () => {
@@ -10,8 +11,9 @@ describe("Create User Presenter Tests", () => {
       favoriteGenres: "HQ",
       favoriteBook: "1984",
     };
+    const repo = new UserRepositoryMock();
     const event = new HttpRequest(example);
-    const response = await createUserPresenter(event);
+    const response = await createUserPresenter(event, repo);
     expect(response.statusCode).toEqual(201);
     expect(response.body.message).toEqual("the user was created");
   });
