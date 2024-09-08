fd286a4d7d832f324e01d48d28fd4cf9cf1d76d1
nicmf <100390575+RaphaelRAY@users.noreply.github.com>
2024-09-07 22:48:32 -0300

    fix(minhaestante): footer element in index.js

--- front/minhaestante/src/index.js
+++ front/minhaestante/src/index.js
@@ -23,17 +23,17 @@ const App = () => {
 				<Route path="/:userId" element={<h1>User Profile</h1>} />
 				<Route path="*" element={<h1>Not Found</h1>} />
 			</Routes>
-			<footer class="background-default text-center">
-				<div class="container">
-					<div class="row">
-						<div class="col">
+			<footer className="background-default text-center">
+				<div className="container">
+					<div className="row">
+						<div className="col">
 							<p>
-								<i class="fa-solid fa-kiwi-bird" style="color: #ffffff;"></i>
+								<i style={{ color: "#ffffff" }}></i>
 								<a
 									href="/test/d5135e3e-646a-55e7-a38d-9724159b7f9f"
 									target="_blank"
 									rel="noreferrer"
-									style="color: white"
+									style={{ color: "white" }}
 								>
 									Para ter acesso a todas as telas clique aqui!
 								</a>
